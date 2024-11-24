0e5c507e2c9fce11cfd5cae0a74b71a5f69bc26c
Tomás Casacão <tomascasacao@gmail.com>
2024-11-24 01:34:23 +0000

    Made nvim background transparent

--- .config/nvim/lua/casacao/plugins/catppucin.lua
+++ .config/nvim/lua/casacao/plugins/catppucin.lua
@@ -1,11 +1,11 @@
 return {
-    {
-        "catppuccin/nvim", 
-        lazy = false,
-        name = "catppuccin", 
-        priority = 100000000,
-        config = function()
-            vim.cmd.colorscheme "catppuccin-macchiato"
-        end
-    }
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 100000000,
+    config = function()
+        require("catppuccin").setup({
+            transparent_background = true,
+        })
+        vim.cmd.colorscheme "catppuccin-macchiato"
+    end
 }
