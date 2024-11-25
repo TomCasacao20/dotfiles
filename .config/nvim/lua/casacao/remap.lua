60d4b16ac4688ebe10f9fcbedfcc2c5900725d1e
Tomás Casacão <tomascasacao@gmail.com>
2024-11-25 18:51:30 +0000

    Nvim modern movement keybinds

--- .config/nvim/lua/casacao/remap.lua
+++ .config/nvim/lua/casacao/remap.lua
@@ -3,4 +3,10 @@ local api = vim.api
 
 vim.g.mapleader = " "
 
+keymap.set({ "n", "v", "o" }, "j", "h")
+keymap.set({ "n", "v", "o" }, "k", "j")
+keymap.set({ "n", "v", "o" }, "l", "k")
+keymap.set({ "n", "v", "o" }, "ç", "l")
+keymap.set({ "n", "v", "o" }, ";", "l")
+
 vim.keymap.set("n", "<space>fb", ":Telescope file_browser path=%:p:h select_buffer=true<CR>")
