3e492698a6d77bf53f71013bd0111a547f2a5f3e
Tomás Casacão <tomascasacao@gmail.com>
2024-12-04 11:59:27 +0000

    added wichkey to nvim config and updated plugins

--- .config/nvim/lua/casacao/remap.lua
+++ .config/nvim/lua/casacao/remap.lua
@@ -8,10 +8,13 @@ keymap.set("n", "<A-l>", ":m .-2<CR>==")
 keymap.set("v", "<A-k>", ":m '>+1<CR>gv=gv")
 keymap.set("v", "<A-l>", ":m '<-2<CR>gv=gv")
 
+keymap.set("n", "<C-d>", "<C-d>zz")
+keymap.set("n", "<C-u>", "<C-u>zz")
+
 keymap.set({ "n", "v", "o" }, "j", "h")
 keymap.set({ "n", "v", "o" }, "k", "j")
 keymap.set({ "n", "v", "o" }, "l", "k")
 keymap.set({ "n", "v", "o" }, "ç", "l")
 keymap.set({ "n", "v", "o" }, ";", "l")
 
-vim.keymap.set("n", "<space>fb", ":Telescope file_browser path=%:p:h select_buffer=true<CR>")
+keymap.set("n", "<space>fb", ":Telescope file_browser path=%:p:h select_buffer=true<CR>")
